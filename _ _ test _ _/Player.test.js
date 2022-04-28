0bd83c54c2d79d7bf2fcfd9e86bf876ea93bb68b
Brittney <brittneyrichardson8@gmail.com>
2022-04-28 09:11:55 -0500

    tested and build player health object

--- _ _ test _ _/Player.test.js
+++ _ _ test _ _/Player.test.js
@@ -32,3 +32,39 @@ test('gets inventory from player or returns false', () => {
 
   expect(player.getInventory()).toEqual(false);
 });
+
+
+//We chose to create a Player method for this in order to help declutter the logic in Game. This way, the game doesn't have to include logic about concatenating player data.
+test("gets player's health value", () => {
+  const player = new Player('Dave');
+
+  //The expect.stringContaining() method is an expect method that we can use to make sure our string includes our player's health. This is preferred in this case because we might need flexibility to change how the player's health will be displayed. This way, if that change happens, we won't need to update our test as well.
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+//Here, we're updating the value of our Player health halfway through the test so that we can check for both conditions: true and false.
+
+test('checks if player is alive or not', () => {
+  const player = new Player('Dave');
+
+  //Remember, truthy values are values that will be coerced to true in Boolean contexts, such as inside if statements.
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+// In this case, we will call the reduceHealth() method twice—the second time with an absurdly high value to make sure that it never goes negative.
+test("subtracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
+});
